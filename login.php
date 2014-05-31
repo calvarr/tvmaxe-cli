28c4b665db2b8215de3fcc05db26769ff721ef94
calvarr <adrianistor@gmail.com>
2014-05-31 19:45:07 +0300

    31.05.2014 - Adus zfilmeonline.org cu suport mail.ru, cinemaxx tot nerezolvat ramine

---
+++ login.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<html lang="ro">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>Conectare - Vizioneaza filme gratuit - www.cinemaxx.ro</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="title" content="Conectare - Vizioneaza filme gratuit - www.cinemaxx.ro"/>
+<meta name="keywords" content=""/>
+<meta name="description" content=""/>
+<link rel="shortcut icon" href="http://www.cinemaxx.ro/uploads/favicon.ico">
+<link type="text/css" rel="stylesheet" href="http://www.cinemaxx.ro/templates/CinemaXes/css/essence_blue.css">
+<link href='//fonts.googleapis.com/css?family=Ubuntu:400,700,400italic,700italic|Open+Sans:400italic,700italic,400,600,700' rel='stylesheet' type='text/css'>
+<script src="http://www.cinemaxx.ro/templates/CinemaXes/js/modernizr-2.6.2.min.js"></script>
+<script type="text/javascript">
+ var MELODYURL = "http://www.cinemaxx.ro";
+ var MELODYURL2 = "http://www.cinemaxx.ro";
+ var TemplateP = "http://www.cinemaxx.ro/templates/CinemaXes";
+ var _LOGGEDIN_ =  false ;
+</script>
+</head>
+<body>
+<header id="pageheader">
+<div id="header">
+<p id="logo">
+<button id="searchtoggle">&#57384;</button>
+<a href="http://www.cinemaxx.ro/index.html" rel="home"><img src="http://www.cinemaxx.ro/uploads/custom-logo.png" alt="Vizioneaza filme gratuit - www.cinemaxx.ro" title="Vizioneaza filme gratuit - www.cinemaxx.ro" border="0"/></a>
+<button id="menutoggle">&#57689;</button>
+</p>
+<form action="http://www.cinemaxx.ro/search.php" method="get" id="search">
+<fieldset>
+<input type="text" name="keywords" id="appendedInputButton" placeholder="Cauta"/>
+<button class="searchbtn">&#57384;</button>
+</fieldset>
+<div id="suggestions">
+<ul id="autoSuggestionsList"></ul>
+</div>
+</form>
+<nav>
+<ul>
+<li><a href="http://www.cinemaxx.ro/index.html">Acasa</a></li>
+<li class="hasdropdown"><a href="#">Categoria <i class="arrow"></i></a>
+<ul id='ul_categories'>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-actiune-online-videos-1-date.html" class="">Actiune</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-animatie-online-videos-1-date.html" class="">Animatie</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-aventura-online-videos-1-date.html" class="">Aventura</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-biografie-online-videos-1-date.html" class="">Biografie</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-comedie-online-videos-1-date.html" class="">Comedie</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-crima-online-videos-1-date.html" class="">Crima</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-documentar-online-videos-1-date.html" class="">Documentar</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-dragoste-online-videos-1-date.html" class="">Dragoste</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-drama-online-videos-1-date.html" class="">Drama</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-familie-online-videos-1-date.html" class="">Familie</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-fantezie-online-videos-1-date.html" class="">Fantezie</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-romanesti-online-videos-1-date.html" class="">Filme Romanesti</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-groaza-online-videos-1-date.html" class="">Groaza</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-istoric-online-videos-1-date.html" class="">Istoric</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-mister-online-videos-1-date.html" class="">Mister</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-muzica-online-videos-1-date.html" class="">Muzical</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-razboi-online-videos-1-date.html" class="">Razboi</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-sf-online-videos-1-date.html" class="">SF</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-sport-online-videos-1-date.html" class="">Sport</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-thriller-online-videos-1-date.html" class="">Thriller</a></li>
+<li class=""><a href="http://www.cinemaxx.ro/browse-filme-western-online-videos-1-date.html" class="">Western</a></li>
+</ul>
+</li>
+<li class="hasdropdown"><a href="#">Articole <i class="arrow"></i></a>
+<ul id='ul_categories'>
+<li><a href="http://www.cinemaxx.ro/article.html">Ultimele articole</a></li><li><a href="http://www.cinemaxx.ro/articles/popular-1.html">Cele mai citite</a></li><li class=""><a href="http://www.cinemaxx.ro/articles/browse-fag-1.html" class="">F.A.Q</a></li>
+</ul></li>
+<li><a href="http://cinemaxx.ro/topvideos.html?do=rating">Top Filme</a></li>
+<li><a href="http://www.cinemaxx.ro/newvideos.html">Filme Noi</a></li>
+<li><a href="http://www.cinemaxx.ro/contact_us.html">Contact</a></li> <li class="mobileonly"><a href="http://www.cinemaxx.ro/login.html">Inregistreaza cont/Conectare</a></li>
+</ul>
+</nav>
+<a href="http://www.cinemaxx.ro/login.html" id="register">Inregistreaza cont/Conectare</a> <div class="hide" id="notification_temporary_display_container"></div>
+</div>
+</header>
+<section class="ad ad-desktop">
+<div class="wrapper">
+<script type="text/javascript">
+var adParams = {
+  a: '11878125', numOfTimes: '5',duration: '1',serverdomain: 's.m2pub.com' ,period: 'day'  , context:'c11845145'
+};
+</script>
+<script type="text/javascript" src="http://creative.m2pub.com/matomy/scripts/popunder/popunder.js"></script>
+</div>
+</section>
+<section id="content">
+<div class="wrapper">
+<div class="right">
+<ul class="subnav">
+<li><a href="http://www.cinemaxx.ro/register.html">Creeaza un cont</a></li>
+<li class="active"><a href="http://www.cinemaxx.ro/login.html">Conectare</a></li>
+<li><a href="http://www.cinemaxx.ro/login.html?do=forgot_pass">Ai uitat parola?</a></li>
+</ul>
+</div>
+<div class="main">
+<h1>Conectare</h1>
+<form name="login_form" id="login-form" method="post" action="http://www.cinemaxx.ro/login.php">
+<ul>
+<li><input type="text" class="text" name="username" value="" placeholder="E-mail sau utilizator"/></li>
+<li><input type="password" class="text" class="input-large" id="pass" name="pass" maxlength="32" autocomplete="off" placeholder="Parola"/></li>
+<li><input type="checkbox" name="remember" value="1"> <small>Tine-ma minte</small></li>
+<li><input type="submit" value="Conectare" class="button loginlogo" name="Login"/></li>
+</ul>
+<a href="http://www.cinemaxx.ro/login.html?do=forgot_pass">Ai uitat parola?</a><br/>
+<a href="http://www.cinemaxx.ro/register.html">Inregistreaza cont</a>
+</form><br>
+<div style='text-align:left'>Daca nu va puteti conecta sau inregistra va rugam sa folositi <br>conectarea prin Facebook accesand optiunea de mai jos :</div> <br><a href="http://www.cinemaxx.ro/fbconnect.php?init" style="float:left" rel="nofollow" title="Register/Login with Facebook"><img src="http://www.cinemaxx.ro/templates/CinemaXes/img/fblogin_small.gif" style="border:none;"/></a><br><br> </div>
+</div>
+</section>
+<footer>
+<div id="footerwrapper">
+<p class="fleft">&copy; 2014 Vizioneaza filme gratuit - www.cinemaxx.ro. Toate drepturile rezervate</p>
+<div id="lang_selector" class="hasdropdown">
+<a class="lang_selected" href="#">
+<img src="http://www.cinemaxx.ro/lang/flags/ro.png" width="16" height="11" border="0" alt="Rom&#226;n&#259;" title="Rom&#226;n&#259;" align="absmiddle"/>
+<span class="arrow"></span>
+</a>
+<ul class="lang_submenu">
+<li>
+<a href="#" title="English" name="1" id="langitem_select_1"><img src="http://www.cinemaxx.ro/lang/flags/us.png" width="16" height="11" border="0" alt="English" title="English"/> English</a>
+</li>
+<li>
+<a href="#" title="Albanian" name="2" id="langitem_select_2"><img src="http://www.cinemaxx.ro/lang/flags/al.png" width="16" height="11" border="0" alt="Albanian" title="Albanian"/> Albanian</a>
+</li>
+<li>
+<a href="#" title="Arabic" name="3" id="langitem_select_3"><img src="http://www.cinemaxx.ro/lang/flags/sa.png" width="16" height="11" border="0" alt="Arabic" title="Arabic"/> Arabic</a>
+</li>
+<li>
+<a href="#" title="Brazilian" name="4" id="langitem_select_4"><img src="http://www.cinemaxx.ro/lang/flags/pt-br.png" width="16" height="11" border="0" alt="Brazilian" title="Brazilian"/> Brazilian</a>
+</li>
+<li>
+<a href="#" title="Bulgarian" name="5" id="langitem_select_5"><img src="http://www.cinemaxx.ro/lang/flags/bg.png" width="16" height="11" border="0" alt="Bulgarian" title="Bulgarian"/> Bulgarian</a>
+</li>
+<li>
+<a href="#" title="Croatian" name="6" id="langitem_select_6"><img src="http://www.cinemaxx.ro/lang/flags/hr.png" width="16" height="11" border="0" alt="Croatian" title="Croatian"/> Croatian</a>
+</li>
+<li>
+<a href="#" title="Danish" name="7" id="langitem_select_7"><img src="http://www.cinemaxx.ro/lang/flags/dk.png" width="16" height="11" border="0" alt="Danish" title="Danish"/> Danish</a>
+</li>
+<li>
+<a href="#" title="Deutsch" name="8" id="langitem_select_8"><img src="http://www.cinemaxx.ro/lang/flags/de.png" width="16" height="11" border="0" alt="Deutsch" title="Deutsch"/> Deutsch</a>
+</li>
+<li>
+<a href="#" title="Fran&#231;ais" name="9" id="langitem_select_9"><img src="http://www.cinemaxx.ro/lang/flags/fr.png" width="16" height="11" border="0" alt="Fran&#231;ais" title="Fran&#231;ais"/> Fran&#231;ais</a>
+</li>
+<li>
+<a href="#" title="Georgian" name="10" id="langitem_select_10"><img src="http://www.cinemaxx.ro/lang/flags/ge.png" width="16" height="11" border="0" alt="Georgian" title="Georgian"/> Georgian</a>
+</li>
+<li>
+<a href="#" title="Italian" name="11" id="langitem_select_11"><img src="http://www.cinemaxx.ro/lang/flags/it.png" width="16" height="11" border="0" alt="Italian" title="Italian"/> Italian</a>
+</li>
+<li>
+<a href="#" title="Lithuanian" name="12" id="langitem_select_12"><img src="http://www.cinemaxx.ro/lang/flags/lt.png" width="16" height="11" border="0" alt="Lithuanian" title="Lithuanian"/> Lithuanian</a>
+</li>
+<li>
+<a href="#" title="Nederlands" name="13" id="langitem_select_13"><img src="http://www.cinemaxx.ro/lang/flags/nl.png" width="16" height="11" border="0" alt="Nederlands" title="Nederlands"/> Nederlands</a>
+</li>
+<li>
+<a href="#" title="Polish" name="14" id="langitem_select_14"><img src="http://www.cinemaxx.ro/lang/flags/pl.png" width="16" height="11" border="0" alt="Polish" title="Polish"/> Polish</a>
+</li>
+<li>
+<a href="#" title="Portuguese" name="15" id="langitem_select_15"><img src="http://www.cinemaxx.ro/lang/flags/pt.png" width="16" height="11" border="0" alt="Portuguese" title="Portuguese"/> Portuguese</a>
+</li>
+<li>
+<a href="#" title="Rom&#226;n&#259;" name="16" id="langitem_select_16"><img src="http://www.cinemaxx.ro/lang/flags/ro.png" width="16" height="11" border="0" alt="Rom&#226;n&#259;" title="Rom&#226;n&#259;"/> Rom&#226;n&#259;</a>
+</li>
+<li>
+<a href="#" title="Russian" name="17" id="langitem_select_17"><img src="http://www.cinemaxx.ro/lang/flags/ru.png" width="16" height="11" border="0" alt="Russian" title="Russian"/> Russian</a>
+</li>
+<li>
+<a href="#" title="Serbian" name="18" id="langitem_select_18"><img src="http://www.cinemaxx.ro/lang/flags/sr.png" width="16" height="11" border="0" alt="Serbian" title="Serbian"/> Serbian</a>
+</li>
+<li>
+<a href="#" title="Slovak" name="19" id="langitem_select_19"><img src="http://www.cinemaxx.ro/lang/flags/sk.png" width="16" height="11" border="0" alt="Slovak" title="Slovak"/> Slovak</a>
+</li>
+<li>
+<a href="#" title="Spanish" name="20" id="langitem_select_20"><img src="http://www.cinemaxx.ro/lang/flags/es.png" width="16" height="11" border="0" alt="Spanish" title="Spanish"/> Spanish</a>
+</li>
+<li>
+<a href="#" title="Swedish" name="21" id="langitem_select_21"><img src="http://www.cinemaxx.ro/lang/flags/se.png" width="16" height="11" border="0" alt="Swedish" title="Swedish"/> Swedish</a>
+</li>
+<li>
+<a href="#" title="T&uuml;rk&ccedil;e" name="22" id="langitem_select_22"><img src="http://www.cinemaxx.ro/lang/flags/tr.png" width="16" height="11" border="0" alt="T&uuml;rk&ccedil;e" title="T&uuml;rk&ccedil;e"/> T&uuml;rk&ccedil;e</a>
+</li>
+<li>
+<a href="#" title="Hebrew" name="23" id="langitem_select_23"><img src="http://www.cinemaxx.ro/lang/flags/il.png" width="16" height="11" border="0" alt="Hebrew" title="Hebrew"/> Hebrew</a>
+</li>
+<li>
+<a href="#" title="Thai" name="24" id="langitem_select_24"><img src="http://www.cinemaxx.ro/lang/flags/th.png" width="16" height="11" border="0" alt="Thai" title="Thai"/> Thai</a>
+</li>
+</ul>
+</div>
+<div class="clearfix"></div>
+</div>
+</footer>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<script src="http://www.cinemaxx.ro/templates/CinemaXes/js/jquery.maskedinput-1.3.min.js" type="text/javascript"></script>
+<script src="http://www.cinemaxx.ro/js/melody.min.js"></script>
+<script type="text/javascript" src="http://www.cinemaxx.ro/templates/CinemaXes/js/essence.min.js"></script>
+<script src="http://www.cinemaxx.ro/templates/CinemaXes/js/waypoints.min.js"></script>
+<script src="http://www.cinemaxx.ro/templates/CinemaXes/js/jquery.cookee.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+
+
+ var pm_lang = {
+	choose_category: "Alege o categorie",
+ 	validate_select_file: "Nici un fisier nu a fost incarcat/selectat",
+ 	validate_video_title: "Please enter a title for this video",
+	onpage_delete_favorite_confirm: "Esti sigur ca vrei sa stergi acest video? Apasa 'Cancel' sa anulezi sau 'OK' sa stergi.",
+	please_wait: "Loading...",
+
+
+}
+
+</script>
+<script src="http://www.cinemaxx.ro/templates/CinemaXes/js/jquery.typewatch.js" type="text/javascript"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+                // live search
+                $('#appendedInputButton').typeWatch({
+                        callback: function() {
+                                        var a = $('#appendedInputButton').val();
+                                        if(a.length < 4){
+                                        	$('#suggestions').hide();
+                                        	return;
+                                        }
+                                        $.ajax({
+                                                type: "POST",
+                         url: MELODYURL2 + "/ajax_search.php",
+                         data: {
+                                                        "queryString": a
+                         },
+                         dataType: "html",
+                         success: function(b){
+                                                        if (b.length > 0) {
+                         $("#suggestions").show();
+                         } else {
+                                                                $("#suggestions").hide();
+                                                        }
+                                                        $("#autoSuggestionsList").html(b);                
+                                                }
+                                        });
+                                },
+                         wait: 400,
+                         highlight: true,
+                         captureLength: 0
+                });
+});
+</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-8346822-4', 'cinemaxx.ro');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
